bbaf7f16ec14455d22ce34b69b7b2320551fc1ca
Balint Szabo <balint.szabo@vc-trade.de>
2020-05-08 09:57:55 +0200

    initial commit

---
+++ src/main/kotlin/hu/bszabo/mave/kotlin/inspections/Hello.kt
@@ -0,0 +1,7 @@
+package hu.bszabo.mave.kotlin.inspections
+
+fun main(args: Array<String>) {
+    val unusedVariable = 1
+    println("Hello, World");
+}
+
---
+++ src/test/kotlin/hu/bszabo/maven/kotlin/inspections/HelloTest.kt
@@ -0,0 +1,8 @@
+package hu.bszabo.maven.kotlin.inspections
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
